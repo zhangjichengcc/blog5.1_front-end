a9b741ab107981b4460c83efb3f5064e4a1a1561
zhangjicheng <zhangjichengcc@163.com>
2025-01-09 18:49:33 +0800

    add less config

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
+import '@/styles/global.less';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <a href="f">aa</a>
-    <a href="f">aa</a>
   </StrictMode>,
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
+// https://cn.vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      less: {
+        additionalData: `@import '/src/styles/variables.less';`,
+        // 允许 Less 使用 Vite 配置的别名
+        paths: '/src',
+        javascriptEnabled: true, // 启用 Less 的 JS 支持
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 });
